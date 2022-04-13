eb3a8675ed5e26d57af1be08923bd55a079114c5
Robert Slocumbe <rslocumbe93@gmail.com>
2022-04-12 21:07:18 -0400

    timer element added

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,9 @@ const quizEl = document.getElementById("quizpage");
 const quizHeaderEl = document.getElementById("quiz-header");
 const choicesEl = document.getElementById("choices-container");
 const proceedEl = document.getElementById("proceed-container");
+const questionEl = document.getElementById("question");
+const timerEl = document.getElementById("timer");
+
 
 // Nested arrays for easier reference
 const questionsArray = [
@@ -54,7 +57,8 @@ const questionsArray = [
 // Variables used for calling unique questions and choices each time displayQuestion() is called
 let currentQuestion = 0;
 let currentChoice = 1;
-let timer = 60;
+let time = 60;
+let timerID; // Needed to identify
 
 
 //////////// QUIZ PAGE FUNCTIONS /////////////////
@@ -63,22 +67,34 @@ let timer = 60;
 const startQuiz = () => {
     homeEl.style.display = "none";
     quizEl.style.display = "block";
+
     displayQuestion();
+
+    timerID = setInterval(countDown, 10); // Runs 
+};
+
+// Function that will run every 10 milliseconds
+const countDown = () => {
+    timerEl.textContent = `Score: ${time.toFixed([2])}`; // toFixed keeps it confined to 2 decimal places
+    time -= 0.01;
+    if (time === 0) {
+        endQuiz();
+    }
 }
 
 // Function used each time a new question needs to be pulled
 const displayQuestion = () => {
-    // Creates and appends question
-    const questionEl = document.createElement("h3");
+
+    // Creates and appends question and timer
+    console.log(questionsArray[currentQuestion][0])
     questionEl.textContent = questionsArray[currentQuestion][0];
-    quizHeaderEl.prepend(questionEl);
 
     // Creates and appends 4 choices
     for (i = 0; i < 4; i++) {
         const choiceBtn = document.createElement("button");
         choiceBtn.textContent = `${currentChoice}. ${questionsArray[currentQuestion][currentChoice]}`;
         choiceBtn.setAttribute("data-number", currentChoice)
-        choiceBtn.addEventListener("click", answerResponse)
+        choiceBtn.addEventListener("click", answerResponse);
         choicesEl.append(choiceBtn);
         
         currentChoice++; // Increases currentChoice so that each button is unique
@@ -91,6 +107,8 @@ const displayQuestion = () => {
 
 // Function that reacts to the users selection
 const answerResponse = event => {
+    clearInterval(timerID); // Pauses the timer
+
     const choice = event.currentTarget;
     const choiceValue = event.currentTarget.getAttribute("data-number");
     const answer = questionsArray[currentQuestion][5];
@@ -104,21 +122,30 @@ const answerResponse = event => {
 
     currentQuestion++;
     createNextBtn(); 
-    // pauseTimer();
 }
 
+// Shows result of users choice and adds next button
 const createNextBtn = () => {
     const nextBtn = document.createElement("button");
     nextBtn.textContent = "NEXT";
     nextBtn.addEventListener("click", () => {
-        quizHeaderEl.removeChild(quizHeaderEl.firstChild); // Removes question but keeps timer
+        quizHeaderEl.textContent = ""; // Clears question
         clearChildren(choicesEl); // Clears choices
-        displayQuestion(); // Sets next question
+
+        // Sets next question unless last question
+        if (currentQuestion < questionsArray.length) {
+            displayQuestion();
+            timerID = setInterval(countDown, 10);
+        } else {
+            endQuiz();
+        }
+
         clearChildren(proceedEl); // Clears next button
     });
     proceedEl.append(nextBtn);
 }
 
+// Used to remove all button elements from the page
 const clearChildren = containerEl => {
     while (containerEl.firstChild) {
         containerEl.removeChild(containerEl.firstChild);
